37d81ceb147e49bc7f64fcc197baed7d8d0fe9df
Denis Zolotarev <judaeus@gmail.com>
2024-02-07 13:51:43 +0200

    * improve App.java

--- src/main/java/ru/dzmakats/App.java
+++ src/main/java/ru/dzmakats/App.java
@@ -18,7 +18,24 @@ public class App {
 
         long id = 1L;
         Customer customer = customerService.get(id);
+        System.out.println("Show customer with id = " + id + ":");
         System.out.println(customer);
 
+        //save old password
+        String oldPassword = customer.getPassword();
+
+        //set new password
+        customer.setPassword("newPassword");
+
+        // update customer in repo
+        customerService.update(customer);
+
+        customer = customerService.get(1L);
+        System.out.println("Show updated customer id = " + id + ":");
+        System.out.println(customer);
+
+        // restore old password and save in repo
+        customer.setPassword(oldPassword);
+        customerService.update(customer);
     }
 }
