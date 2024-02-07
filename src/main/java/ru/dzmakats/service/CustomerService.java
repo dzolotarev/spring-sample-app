b34a7929b1435d271300dbc0caa86cc4edf3f1cd
Denis Zolotarev <judaeus@gmail.com>
2024-02-07 16:45:16 +0200

    * attach Spring Framework: now everything is in the AppContext

--- src/main/java/ru/dzmakats/service/CustomerService.java
+++ src/main/java/ru/dzmakats/service/CustomerService.java
@@ -1,15 +1,20 @@
 package ru.dzmakats.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import ru.dzmakats.entity.Customer;
 import ru.dzmakats.repository.Repo;
 
 /**
  * Created by Denis Zolotarev on 06.02.2024
  */
+
+@Service
 public class CustomerService {
 
     private Repo<Customer> customerRepo;
 
+    @Autowired
     public void setCustomerRepo(Repo<Customer> customerRepo) {
         this.customerRepo = customerRepo;
     }
