21df5c08c1933287cb673a8a4c5674c6c5d96bb9
Denis Zolotarev <judaeus@gmail.com>
2024-02-08 18:02:48 +0200

    * refactor postProcessBeforeInitialization() method

--- src/main/java/ru/dzmakats/processor/Benchmark.java
+++ src/main/java/ru/dzmakats/processor/Benchmark.java
@@ -1,6 +1,8 @@
 package ru.dzmakats.processor;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
@@ -8,5 +10,6 @@ import java.lang.annotation.Target;
  */
 
 @Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Benchmark {
 }
--- src/main/java/ru/dzmakats/processor/BenchmarkAnnotationBeanPostProcessor.java
+++ src/main/java/ru/dzmakats/processor/BenchmarkAnnotationBeanPostProcessor.java
@@ -4,6 +4,8 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+
 /**
  * Created by Denis Zolotarev on 08.02.2024
  */
@@ -12,8 +14,19 @@ import org.springframework.stereotype.Component;
 public class BenchmarkAnnotationBeanPostProcessor implements BeanPostProcessor {
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
-        System.out.printf(">>>> Before init %s%n", beanName);
+
+        Class<?> aClass = bean.getClass();
+        if (aClass.isAnnotationPresent(Benchmark.class) || methodIsAnnotationPresent(aClass)) {
+            System.out.printf(">>>> Before init %s%n", beanName);
+        }
         return bean;
+    }
+
+    private boolean methodIsAnnotationPresent(Class<?> aClass) {
+        return Arrays.stream(aClass.getMethods())
+                .filter(method -> method.isAnnotationPresent(Benchmark.class))
+                .findFirst()
+                .isPresent();
     }
 
     @Override
